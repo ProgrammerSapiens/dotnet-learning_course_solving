c27e7d4e65718b94a3c2cbf25aa032b981c22bdc
Dmitriy <homyakov-dmitriy@mail.ru>
2024-06-19 15:49:40 +0200

    Реализация класса Deal, добавление десериализации из файла json

---
+++ Lesson_1_Part_1/Deal.cs
@@ -0,0 +1,23 @@
+﻿namespace Lesson_1_Part_1
+{
+    /// <summary>
+    /// Represets a Deal with a sum, id, and Date 
+    /// </summary>
+    internal class Deal
+    {
+        /// <summary>
+        /// Gets or sets the id of the deal 
+        /// </summary>
+        public string Id { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the sum of the deal
+        /// </summary>
+        public int Sum { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date of the deal
+        /// </summary>
+        public DateTime Date { get; set; }
+    }
+}
