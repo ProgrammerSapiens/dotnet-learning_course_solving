6e40aeec341e021ebff96a9a02d00aecbe2b8e35
ProgrammerSapiens <120135723+ProgrammerSapiens@users.noreply.github.com>
2024-06-23 20:08:06 +0200

    Update Program.cs
    Замена API токена на нейтральную надпись

--- Lesson_1_Part_2/Program.cs
+++ Lesson_1_Part_2/Program.cs
@@ -6,7 +6,7 @@ namespace Lesson_1_Part_2
 {
     internal class Program
     {
-        static private readonly string API_KEY = "2b368ffa7bac7224db94e26166783c65491a4c54";
+        static private readonly string API_KEY = "Enter your token";
 
         static async Task Main(string[] args)
         {
