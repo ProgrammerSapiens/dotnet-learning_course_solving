8a07f01a578c0df25552a0d3a19464d15626be48
Dmitriy <homyakov-dmitriy@mail.ru>
2024-07-04 15:00:28 +0200

    Реализация приложения на ASP.NET Core, которое отдает ответ по HTTP Get-запросу аналогично Уроку 1.Часть 2.

---
+++ CompanySearchApi/Controllers/CompanyController.cs
@@ -0,0 +1,42 @@
+﻿using Dadata;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanySearchApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly string API_KEY = "2b368ffa7bac7224db94e26166783c65491a4c54";
+        private readonly SuggestClientAsync _api;
+
+        public CompanyController()
+        {
+            _api = new SuggestClientAsync(API_KEY);
+        }
+
+        [HttpGet("{inn}")]
+        public async Task<IActionResult> GetCompanyByInn(string inn)
+        {
+            if (string.IsNullOrEmpty(inn))
+            {
+                return BadRequest("The INN cannot be empty.");
+            }
+
+            try
+            {
+                var result = await _api.FindParty(inn);
+                if (result.suggestions.Count == 0)
+                {
+                    return NotFound("Conpany does not exist.");
+                }
+
+                return Ok(result.suggestions);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ CompanySearchApi/Startup.cs
@@ -0,0 +1,38 @@
+﻿namespace CompanySearchApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseCors("AllowAll");
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
