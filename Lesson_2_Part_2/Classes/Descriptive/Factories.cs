8b32b61054dee7742af414e571ec9151314f8a38
Dmitriy <homyakov-dmitriy@mail.ru>
2024-08-12 10:50:53 +0200

    Реализация интерфейса, объединяющего классы Factory, Unit, Tank

---
+++ Lesson_2_Part_2/Classes/Descriptive/Factories.cs
@@ -0,0 +1,43 @@
+﻿using Lesson_2_Part_2.Interfaces;
+
+namespace Lesson_2_Part_1.Classes.Descriptive
+{
+    /// <summary>
+    /// Represents a factory with an Id, name, and description
+    /// </summary>
+    internal class Factories : IHaveName
+    {
+        /// <summary>
+        /// Gets or sets the Id of the factory
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the factory
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the description of the factory
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Factories"/> class.
+        /// </summary>
+        public Factories() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Factories"/> class with the specified Id, name, and description
+        /// </summary>
+        /// <param name="id">The ID of the factory</param>
+        /// <param name="name">The name of the factory</param>
+        /// <param name="description">The description of the factory</param>
+        public Factories(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+    }
+}
---
+++ Lesson_2_Part_2/Interfaces/IHaveName.cs
@@ -0,0 +1,9 @@
+﻿namespace Lesson_2_Part_2.Interfaces
+{
+    public interface IHaveName
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
