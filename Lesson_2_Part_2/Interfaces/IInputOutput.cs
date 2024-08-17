7698cc686d58fbedf27fcd889580178ef3bae49f
Dmitriy <homyakov-dmitriy@mail.ru>
2024-08-17 11:06:47 +0200

    Добавление интерфейса для ввода-вывода

---
+++ Lesson_2_Part_2/Interfaces/IInputOutput.cs
@@ -0,0 +1,12 @@
+﻿using Lesson_2_Part_2.Classes.Descriptive;
+
+namespace Lesson_2_Part_2.Interfaces
+{
+    public interface IInputOutput
+    {
+        public event Action<string> onMessageReceived;
+        void ReceiveMessage(string message);
+        void ShowOneTank(IReadOnlyCollection<Factories> factories, IReadOnlyCollection<Units> units, IReadOnlyCollection<Tanks> tanks);
+        void ShowTanksInfo(IReadOnlyCollection<Factories> factories, IReadOnlyCollection<Units> units, IReadOnlyCollection<Tanks> tanks);
+    }
+}
