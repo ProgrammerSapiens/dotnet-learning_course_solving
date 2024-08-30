5f33c9ee00e0997180e729a04e87fe5e1af2d260
Dmitriy <homyakov-dmitriy@mail.ru>
2024-08-30 10:40:30 +0200

    Дополнительное задание на поиск сдвига чисел в массиве

---
+++ HowBigIsTheShift/HowBigIsTheShift/NumericShiftDetector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowBigIsTheShift
+{
+    public class NumericShiftDetector
+    {
+        private int[] nums;
+
+        public int GetShiftPosition(int[] initialArray)
+        {
+            nums = initialArray;
+            int length = initialArray.Length;
+            if (initialArray[0] < initialArray[length - 1]) return 0;
+            return findShift(0, length - 1);
+        }
+
+        public int findShift(int left, int right)
+        {
+            if (left == right) return left;
+            int mid = (left + right) / 2;
+
+            if (nums[mid] > nums[mid + 1]) return mid + 1;
+            if (nums[mid] < nums[mid - 1]) return mid;
+
+            if (nums[mid] > nums[right]) return findShift(mid + 1, right);
+            else return findShift(left, mid);
+        }
+    }
+}
---
+++ HowBigIsTheShift/HowBigIsTheShift/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HowBigIsTheShift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
